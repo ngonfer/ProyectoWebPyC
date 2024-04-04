b57ea0baa215b03f913c9945bae0e59de96bf44b
Nicolás <ngonfer142@g.educaand.es>
2024-04-04 19:36:54 +0200

    Actualizada la base de datos

--- sql/BaseDeDatos.sql
+++ sql/BaseDeDatos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Producto (
-  Código_Tipo varchar(4),
+  Cod_Tipo varchar(4),
   Nombre enum('Aceite de oliva virgen extra', 'Aceite de oliva virgen', 'Aceite de orujo de oliva') NOT NULL,
   Precio float NOT NULL,
-  CONSTRAINT Código_Tipo_pk PRIMARY KEY(Código_Tipo)
+  CONSTRAINT Código_Tipo_pk PRIMARY KEY(Cod_Tipo)
 );
 
 CREATE TABLE Socio (
@@ -37,15 +37,49 @@ CREATE TABLE Entrega (
 CREATE TABLE Venta (
   Num_Factura integer Auto_Increment,
   Fecha_y_hora timestamp NOT NULL,
+  NIF_Socio varchar(9),
+  CONSTRAINT Venta_NIF_Socio_fk FOREIGN KEY(NIF_Socio)
+                        REFERENCES Socio(NIF)
+                        ON UPDATE CASCADE
+                        ON DELETE NO ACTION,
   CONSTRAINT Num_Factura_pk PRIMARY KEY(Num_Factura)
 );
 
-INSERT INTO Producto VALUES (0001, 'Aceite de oliva virgen extra', 12.50);
-INSERT INTO Producto VALUES (0002, 'Aceite de oliva virgen', 10.30);
-INSERT INTO Producto VALUES (0003, 'Aceite de orujo de oliva', 5.40);
-INSERT INTO Socio VALUES ('AAAAAAAAM', 'Paquito', 'Perez', 'Montalvo', 'Calle', '23433', 'Jaen', 'Jaen', '23131244', 'fpef@gmail.com');
-INSERT INTO Entrega VALUES (NULL, 2, '2024-03-12 07:16:43', 'Árbol', 56, 42, 'AAAAAAAAM');
+CREATE TABLE Producto_Venta (
+  Num_Factura integer,
+  Cod_Tipo varchar(4),
+  CONSTRAINT Producto_Venta_Factura_fk FOREIGN KEY(Num_Factura)
+                                       REFERENCES Venta(Num_Factura)
+                                       ON UPDATE CASCADE
+                                       ON DELETE NO ACTION,
+  CONSTRAINT Producto_Venta_Tipo_fk FOREIGN KEY(Cod_Tipo)
+                                       REFERENCES Producto(Cod_Tipo)
+                                       ON UPDATE CASCADE
+                                       ON DELETE NO ACTION,
+  CONSTRAINT Num_Factura_pk PRIMARY KEY(Num_Factura, Cod_Tipo)
+);
 
-SELECT * FROM Producto;
+INSERT INTO Producto VALUES ('0001', 'Aceite de oliva virgen extra', 12.50);
+INSERT INTO Producto VALUES ('0002', 'Aceite de oliva virgen', 10.30);
+INSERT INTO Producto VALUES ('0003', 'Aceite de orujo de oliva', 5.40);
+
+INSERT INTO Socio VALUES ('11111111A', 'Paquito', 'Perez', 'Montalvo', 'Calle Alcalá', '23433', 'Jaen', 'Jaen', '23131244', 'paquitoperez@gmail.com');
+INSERT INTO Socio VALUES ('22222222B', 'Juan', 'Gomez', 'Moya', 'Calle de los ciegos', '23439', 'Jaen', 'Jaen', '23183456', 'juangomez@gmail.com');
+INSERT INTO Socio VALUES ('33333333C', 'Lucía', 'López', 'Pascual', 'Paseo de Gracia', '23434', 'Jaen', 'Jaen', '23131364', 'luciaperez@gmail.com');
+
+INSERT INTO Entrega VALUES (NULL, 2, '2024-03-12 07:16:43', 'Árbol', 56, 42, '11111111A');
+INSERT INTO Entrega VALUES (NULL, 3, '2024-03-24 06:19:46', 'Suelo', 102, 45, '22222222B');
+INSERT INTO Entrega VALUES (NULL, 4, '2024-02-26 16:16:36', 'Árbol', 204, 12, '33333333C');
+
+INSERT INTO Venta VALUES (NULL, '2024-02-12 12:16:43','22222222B');
+INSERT INTO Venta VALUES (NULL, '2024-01-11 12:14:43','33333333C');
+INSERT INTO Venta VALUES (NULL, '2024-01-23 12:18:46','11111111A');
+
+-- INSERT INTO Producto_Venta (1, '0001');
+-- INSERT INTO Producto_Venta (2, '0002');
+-- INSERT INTO Producto_Venta (3, '0003');
+
+SELECT Precio FROM Producto WHERE Cod_Tipo='0002';
 SELECT * FROM Entrega;
 SELECT * FROM Socio;
+SELECT * FROM Venta;
