f89e996e6e572643b44700a94a5fb5c2cf0e7bff
Javier Cobo Marchal <jcobmar692@g.educaand.es>
2024-04-23 10:56:36 +0200

    Arreglados un par de errores

--- php/conexión.php
+++ php/conexión.php
@@ -3,7 +3,7 @@
     $nombreServidor = "localhost";
     $baseDatos = "Cooperativa";
     $nombreUsuario = "usuario";
-    $password = "usuario ";
+    $password = "usuario";
    
     // Se crea la conexión
    
@@ -19,18 +19,14 @@
     echo "Se ha realizado la conexión";
 
     $sentenciaSQL="INSERT INTO Producto VALUES ('0004', 'Aceite de oliva virgen extra', 12.50);";
-
-    $conn->query($sentenciaSQL);
  
-    /*if (!$conn->query($sentenciaSQL))
+    if (!$conn->query($sentenciaSQL))
     // ->query($sentenciaSQL)) 
     {
         echo "Ha fallado la inserción de datos";
     }
-*/
-    /*echo "Se ha ejecutado correctamente la inserción de datos";*/
+
+    echo "Se ha ejecutado correctamente la inserción de datos";
 
     echo $sentenciaSQL;
   
-    mysqli_close($conn);
-?>
